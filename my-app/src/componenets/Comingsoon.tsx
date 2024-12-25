e35309d949ee26b5bac481bcb2727b0e47a3c121
Delta Student gaurav <119969595+gaurav1n@users.noreply.github.com>
2024-12-25 19:23:11 +0530

    Made some changes in components

---
+++ my-app/src/componenets/Comingsoon.tsx
@@ -0,0 +1,26 @@
+
+// import { BsHourglassSplit } from "react-icons/bs"; // Hourglass icon
+
+
+// function UnderConstruction() {
+//   return (
+//     <div className="flex flex-col md:flex-row text-[2rem] gap-2 md:text-[4rem] font-bold justify-center items-center h-[100vh]">
+//       <BsHourglassSplit className="text-[#d3d3c3] w-[5rem] h-[5rem] bg-black md:w-[10rem] md:h-[10rem]"/>
+//       <h1>Coming Soon</h1>
+//     </div>
+//   );
+// }
+
+// export default UnderConstruction;
+import { BsHourglassSplit } from "react-icons/bs";
+
+function UnderConstruction() {
+ return (
+   <div className="flex flex-col md:flex-row text-[2rem] gap-2 md:text-[4rem] font-bold justify-center items-center h-[100vh]">
+     <BsHourglassSplit className="text-[#212121] w-[5rem] h-[5rem] md:w-[10rem] md:h-[10rem]"/>
+     <h1>Coming Soon</h1>
+   </div>
+ );
+}
+
+export default UnderConstruction;
