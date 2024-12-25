700d88afa4257e6ad4b870721c31485ab3b18daa
gaurav1Nn <gauravnilawar26@gmail.com>
2024-12-25 19:30:38 +0530

    Update Comingsoon.tsx

--- my-app/src/componenets/Comingsoon.tsx
+++ my-app/src/componenets/Comingsoon.tsx
@@ -1,20 +1,6 @@
-
-// import { BsHourglassSplit } from "react-icons/bs"; // Hourglass icon
-
-
-// function UnderConstruction() {
-//   return (
-//     <div className="flex flex-col md:flex-row text-[2rem] gap-2 md:text-[4rem] font-bold justify-center items-center h-[100vh]">
-//       <BsHourglassSplit className="text-[#d3d3c3] w-[5rem] h-[5rem] bg-black md:w-[10rem] md:h-[10rem]"/>
-//       <h1>Coming Soon</h1>
-//     </div>
-//   );
-// }
-
-// export default UnderConstruction;
 import { BsHourglassSplit } from "react-icons/bs";
 
-function UnderConstruction() {
+function Comingsoon() {
  return (
    <div className="flex flex-col md:flex-row text-[2rem] gap-2 md:text-[4rem] font-bold justify-center items-center h-[100vh]">
      <BsHourglassSplit className="text-[#212121] w-[5rem] h-[5rem] md:w-[10rem] md:h-[10rem]"/>
@@ -23,4 +9,4 @@ function UnderConstruction() {
  );
 }
 
-export default UnderConstruction;
+export default Comingsoon;
